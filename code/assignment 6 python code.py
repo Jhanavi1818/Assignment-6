f418e8a6bf2be4a0548925d42ce93a71d5fadb59
Jhanavi1818 <102795854+Jhanavi1818@users.noreply.github.com>
2022-05-25 15:29:02 +0530

    Add files via upload

---
+++ code/assignment 6 python code.py
@@ -0,0 +1,9 @@
+#Importing statistics module
+import statistics 
+# creating a sample list
+sample =(y=ax+b)
+#calculating the mean of sample
+m = statistics.mean(sample)
+#calculating the variace of sample
+print("variance of ample set is% s"
+%(statistics.variance(sample.xbar=m)))
